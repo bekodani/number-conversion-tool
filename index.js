ba937b103ea0546790b7704eca5e76d5be94242f
bekodani <bekodani@gmail.com>
2022-04-02 14:52:07 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+let numberConversion = function(num) {
+    let numArrOne = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"];
+    let numArrTwo = ["thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
+    /* let argLength = num.toString().length;
+    return argLength; */
+    return numArr[num-1]
+};
+
+console.log(numberConversion(7))
